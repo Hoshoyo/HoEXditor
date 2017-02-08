65fba60177dddc230ce8353164018149bee6507f
Hoshoyo <pveiga1991@hotmail.com>
2017-02-08 15:45:05 -0200

    start commit

---
+++ build.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set build_type=debug
+
+set floc=..\..\src\
+
+set exename=hoex
+set files=%floc%main.c
+
+set include_dirs= -Iinclude
+set link_libraries_release= kernel32.lib libcmt.lib libvcruntime.lib libucrt.lib libcpmt.lib
+set link_libraries_debug= kernel32.lib libcmtd.lib libvcruntimed.lib libucrtd.lib libcpmtd.lib
+
+set compiler_flags_release= /O2 /MT /Zi /nologo /Fe%exename%.exe
+set compiler_flags_debug= /Od /MTd /Zi /nologo /Fe%exename%.exe
+
+set linker_flags_release= /MACHINE:X64 /incremental:no /NODEFAULTLIB:LIBCMT
+set linker_flags_debug= /MACHINE:X64 /incremental:no /NODEFAULTLIB
+
+if NOT EXIST bin mkdir bin
+pushd bin
+if NOT EXIST %build_type% mkdir %build_type%
+pushd %build_type%
+
+if %build_type% == release (
+	call cl %compiler_flags_release% %include_dirs% %files% /link %linker_flags_release% %link_libraries_release%
+)
+
+if %build_type% == debug (
+	call cl %compiler_flags_debug% %include_dirs% %files% /link %linker_flags_debug% %link_libraries_debug%
+)
+
+popd
+popd
+
+:eof
